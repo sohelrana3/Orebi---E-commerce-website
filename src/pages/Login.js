3c2c843ba6558c5d061d32316662bdb1c411b96b
sohelrana3 <expartdesigns5@gmail.com>
2023-03-25 11:47:14 +0600

    Update Login.js
    Badcam & p tag add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Badcam from '../componet/Lyaouts/Badcam'
+import Container from '../componet/Lyaouts/Container'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+        <Badcam />
+        <div className='w-full border-b border-[#767676] pb-16'>
+            <p className='w-1/3 text-[#767676] font-dm font-normal text-sm'>Lorem Ipsum is simply dummy  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+        </div>
+    </Container>
   )
 }
 
