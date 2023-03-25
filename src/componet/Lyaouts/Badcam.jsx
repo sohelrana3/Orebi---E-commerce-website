e93f83f22d81319ecf52cfd75d4f4c896cf84d8e
sohelrana3 <expartdesigns5@gmail.com>
2023-03-25 13:26:02 +0600

    Login Page Responsiable
    login page pages responsanble

--- src/componet/Lyaouts/Badcam.jsx
+++ src/componet/Lyaouts/Badcam.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Badcam = ({title}) => {
     console.log(window.location.pathname);
   return (
-    <div className='my-28'>
+    <div className='my-10 sm:my-16 md:my-20 lg:my-28'>
         <h1 className='mb-3 font-dm font-bold text-5xl capitalize'>{window.location.pathname.split("/")[1]}</h1>
         <p className='flex items-center gap-x-2 font-normal font-dm text-[#767676]'>
           <Link to='/'>Home</Link><FaAngleRight/> {window.location.pathname.split("/")[1]}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,22 +9,22 @@ const Login = () => {
   return (
     <Container>
         <Badcam />
-        <div className='w-full border-b border-[#F0F0F0] pb-16'>
-            <p className='w-1/3 text-[#767676] font-dm font-normal text-sm'>Lorem Ipsum is simply dummy  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+        <div className='w-full border-b border-[#F0F0F0] pb-5 md:pb-16'>
+            <p className='w-ful lg:w-1/3 text-[#767676] font-dm font-normal text-sm'>Lorem Ipsum is simply dummy  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
         </div>
-        <div className='mt-14'>
+        <div className='mt-5 lg:mt-14'>
             <Headding title='Returning Customer' /> 
-            <div className='flex w-3/4 gap-x-10'>
+            <div className='md:flex w-full lg:w-3/4 gap-x-10'>
                 <Inputbox as='input' title='Email address' placeholder="company@domain.com" />
                 <Inputbox as='input' title='Password' placeholder="******" />
             </div>
-            <div className='mt-7'>
+            <div className='mt-2 sm:max-md:mt-5 lg:mt-7'>
                 <Button title='Login' />
             </div>
-            <div className='mt-32'>
+            <div className='mt-10 sm:mt-16 md:mt-20  lg:mt-32'>
                 <Headding title='New Customer' /> 
-                <p className='w-1/3 text-[#767676] font-dm font-normal text-sm'>Lorem Ipsum is simply dummy  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
-                <div className='mt-12'>
+                <p className='w-full lg:w-1/3 text-[#767676] font-dm font-normal text-sm'>Lorem Ipsum is simply dummy  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                <div className='mt-4 sm:mt-7 md:mt-9 lg:mt-12'>
                     <Button title='Continue' />
                 </div>
             </div>
